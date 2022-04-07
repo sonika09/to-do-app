495b61afedb5a7762295e0795308a38f8d09fed9
sonika09 <sonikasoni0517@gmail.com>
2022-04-07 07:58:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyTaskList from './MyTaskList';
+
+function App() {
+  return (
+    <div className="App">
+     <MyTaskList/>
+    </div>
+  );
+}
+
+export default App;
